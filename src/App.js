3301accc21718c9240de1991565ce3a4790edb13
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2022-10-02 16:42:11 +0200

    fixing path adding exact

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route
-          path=""
+          exact
+          path="/"
           element={
             <>
               <Slider />
